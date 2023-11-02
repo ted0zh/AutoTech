9d06be0a8fbc7c92691ee42f1f047af96cd289f4
Andrian <andriandimitrov02@gmail.com>
2023-11-02 22:27:53 +0200

    popravka

--- autoTech/src/main/java/com/autoTech/autoTech/models/Grades.java
+++ autoTech/src/main/java/com/autoTech/autoTech/models/Grades.java
@@ -21,8 +21,5 @@ public class Grades {
 
     public void setServiceGrade(Double serviceGrade) {this.serviceGrade = serviceGrade;}
 
-    public Grades(Long id, Double serviceGrade) {
-        this.id = id;
-        this.serviceGrade = serviceGrade;
-    }
+
 }
