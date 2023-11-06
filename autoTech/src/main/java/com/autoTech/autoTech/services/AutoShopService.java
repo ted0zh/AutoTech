cff6e0e626f14c42fe5efa226e978aeef0dc1244
pen4aka <penkopenev2002@gmail.com>
2023-11-06 22:31:02 +0200

    Changes by Tedi

--- autoTech/src/main/java/com/autoTech/autoTech/services/AutoShopService.java
+++ autoTech/src/main/java/com/autoTech/autoTech/services/AutoShopService.java
@@ -1,8 +1,10 @@
 package com.autoTech.autoTech.services;
 
+import com.autoTech.autoTech.Mapper.AutoShopMapper;
 import com.autoTech.autoTech.dto.AutoShopDto;
 import com.autoTech.autoTech.models.AutoShop;
 import com.autoTech.autoTech.repositories.AutoShopRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,14 @@ import java.util.Optional;
 
 
 @Service
+@Slf4j
 public class AutoShopService {
     private final AutoShopRepo autoShopRepo;
-
+    private final AutoShopMapper autoShopMapper;
 @Autowired
-    public AutoShopService(AutoShopRepo autoShopRepo) {
+    public AutoShopService(AutoShopRepo autoShopRepo,AutoShopMapper mapper) {
         this.autoShopRepo = autoShopRepo;
+        this.autoShopMapper= mapper;
     }
 
     public List<AutoShop> getAllShops(){
@@ -24,15 +28,28 @@ public class AutoShopService {
     }
     public AutoShop saveShop(AutoShopDto autoShopDto) {
         Optional<AutoShop> dbObject = autoShopRepo.findAutoShopByShopName(autoShopDto.getShopName());
-        AutoShop autoShop = dbObject.orElseGet(AutoShop::new); // Use orElseGet
+        Long id;
+        if(dbObject.isPresent()) {
+            id = dbObject.get().getId();
+            log.info("Updating AutoShop with id {}", id);
+        } else {
+            id = null;
+            log.info("Inserting new AutoShop");
+        }
+        AutoShop autoShop = autoShopMapper.convertDtoToEntity(autoShopDto, id);
+        return autoShopRepo.saveAndFlush(autoShop);
 
-        autoShop.setShopName(autoShopDto.getShopName());
-        autoShop.setEmailShop(autoShopDto.getEmailShop());
-        autoShop.setPhoneNumber(autoShopDto.getPhoneNumber());
-        autoShop.setLocation(autoShopDto.getLocation()); // Correct the property name
-        autoShop.setInfo(autoShopDto.getInfo());
 
-        return autoShopRepo.save(autoShop);
+//        Optional<AutoShop> dbObject = autoShopRepo.findAutoShopByShopName(autoShopDto.getShopName());
+//        AutoShop autoShop = dbObject.orElseGet(AutoShop::new); // Use orElseGet
+//
+//        autoShop.setShopName(autoShopDto.getShopName());
+//        autoShop.setEmailShop(autoShopDto.getEmailShop());
+//        autoShop.setPhoneNumber(autoShopDto.getPhoneNumber());
+//        autoShop.setLocation(autoShopDto.getLocation()); // Correct the property name
+//        autoShop.setInfo(autoShopDto.getInfo());
+//
+//        return autoShopRepo.save(autoShop);
     }
 
 }
