41b613375927b8eb88b8b3792aeae42494094289
ted0zh <tedi200220@gmail.com>
2023-11-15 16:02:50 +0200

    Changes by usr

--- autoTech/src/main/java/com/autoTech/autoTech/controllers/ShopSpecializationController.java
+++ autoTech/src/main/java/com/autoTech/autoTech/controllers/ShopSpecializationController.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/specialization")
 //@CrossOrigin("*")
--- autoTech/src/main/java/com/autoTech/autoTech/repositories/SpecializationsRepo.java
+++ autoTech/src/main/java/com/autoTech/autoTech/repositories/SpecializationsRepo.java
@@ -3,6 +3,7 @@ package com.autoTech.autoTech.repositories;
 
 import com.autoTech.autoTech.models.Specializations;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
