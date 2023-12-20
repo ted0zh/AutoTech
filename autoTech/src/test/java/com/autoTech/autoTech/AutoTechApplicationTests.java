f8b95cc54a25c6e8f3b04262c5d92e9307471686
ted0zh <tedi200220@gmail.com>
2023-12-20 15:34:12 +0200

    test for controller

--- autoTech/src/test/java/com/autoTech/autoTech/AutoTechApplicationTests.java
+++ autoTech/src/test/java/com/autoTech/autoTech/AutoTechApplicationTests.java
@@ -1,13 +1,17 @@
 package com.autoTech.autoTech;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class AutoTechApplicationTests {
 
+	@Autowired
+	private AutoTechApplication underTest;
+
 	@Test
 	void contextLoads() {
+		assertThat(underTest).isNotNull();
 	}
 
 }
